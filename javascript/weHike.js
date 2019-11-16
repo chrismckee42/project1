e19433d973062e1f7fbcef8ef8e6ea040612344a
Christopher McKee <chris.mckee.13@gmail.com>
2019-11-16 13:23:01 -0800

    loads trails based on location search

--- javascript/weHike.js
+++ javascript/weHike.js
@@ -5,30 +5,31 @@ var latlng;
 var longitude;
 var latitude;
 
-(function() {
-    var placesAutocomplete = places({
-      appId: 'pl02J9DTRSCU',
-      apiKey: 'f099e7b02e4b0724d8cdb3f3ff4b3307',
-      container: document.querySelector('#address')
-    });
-  
-    var $address = document.querySelector('#address-value')
-    placesAutocomplete.on('change', function(e) {
-      $address.textContent = e.suggestion.value
-      latlng = e.suggestion.latlng;
-      latitude = latlng.lat;
-      longitude = latlng.lng;
-      console.log(latlng);
-      console.log("latitude: ", latitude);
-      console.log("longitude: ", longitude);
-    });
-  
-    placesAutocomplete.on('clear', function() {
-      $address.textContent = 'none';
+(function () {
+  var placesAutocomplete = places({
+    appId: 'pl02J9DTRSCU',
+    apiKey: 'f099e7b02e4b0724d8cdb3f3ff4b3307',
+    container: document.querySelector('#address')
+  });
 
-    });
-  
-  })();
+  var $address = document.querySelector('#address-value')
+  placesAutocomplete.on('change', function (e) {
+    $address.textContent = e.suggestion.value
+    latlng = e.suggestion.latlng;
+    latitude = latlng.lat;
+    longitude = latlng.lng;
+    console.log(latlng);
+    console.log("latitude: ", latitude);
+    console.log("longitude: ", longitude);
+    getHikeInfo(latitude, longitude, 10)
+  });
+
+  placesAutocomplete.on('clear', function () {
+    $address.textContent = 'none';
+
+  });
+
+})();
 
 
 function getHikeInfo(lat, lon, rad) {
@@ -41,15 +42,15 @@ function getHikeInfo(lat, lon, rad) {
   queryURL += "&sort=" + "quality" //can be quality or distance 
   queryURL += "&minLength=" + 0
   queryURL += "&key=200637782-30288d07ad9589c7fb834a4deb5b5a6e"
-
+  console.log(queryURL)
   //runs query
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    var results = response.data
-    console.log(results[0])
-    $("#inputHikes").clear()
+    var results = response.trails
+
+    $("#inputHikes").empty()
 
     //loops through all the returned trails
     for (let i = 0; i < results.length; i++) {
@@ -60,10 +61,11 @@ function getHikeInfo(lat, lon, rad) {
       hikeDiv.attr("lon", results[i].longitude)
       //adds trail name and distance
       var name = $("<div>").text(results[i].name)
-      var dist = $("<div>").text("Distance: " + results[i].length)
+      var dist = $("<div>").text("Distance: " + results[i].length + " miles")
       //adds trail image
       var trailImage = $("<img>")
-      topicImage.attr("src", results[i].imgSqSmall)
+      trailImage.attr("src", results[i].imgSmall)
+      trailImage.addClass("trail-pic")
       //appends trail to page
       hikeDiv.append(name);
       hikeDiv.append(dist)
