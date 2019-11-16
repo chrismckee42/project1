e970e6e5d6f4db53601d03c9c364ef47ffff0fe2
Danielle <danineubauerr@gmail.com>
2019-11-16 10:54:57 -0800

    adds search function to complete location

--- javascript/weHike.js
+++ javascript/weHike.js
@@ -1,3 +1,22 @@
+(function() {
+    var placesAutocomplete = places({
+      appId: 'pl02J9DTRSCU',
+      apiKey: 'f099e7b02e4b0724d8cdb3f3ff4b3307',
+      container: document.querySelector('#address')
+    });
+  
+    var $address = document.querySelector('#address-value')
+    placesAutocomplete.on('change', function(e) {
+      $address.textContent = e.suggestion.value
+    });
+  
+    placesAutocomplete.on('clear', function() {
+      $address.textContent = 'none';
+    });
+  
+  })();
+
+
 function getHikeInfo(lat, lon, rad) {
 
     var key = "200637782-30288d07ad9589c7fb834a4deb5b5a6e"
